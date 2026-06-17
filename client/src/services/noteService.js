0d77d2933b0b4efd0cc3363f40c64213cb1558f0
divyap1305 <divyapalanisamy1305@gmail.com>
2026-06-17 23:19:11 +0530

    Prepared environment variables for deployment

--- client/src/services/noteService.js
+++ client/src/services/noteService.js
@@ -1,16 +1,8 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:5000/api/notes"
+const API_URL =
+  `${import.meta.env.VITE_API_URL}/notes`
 
-// Get Notes
-export const getNotes = async () => {
-
-  const response = await axios.get(API_URL, getConfig())
-
-  return response.data
-}
-
-// Get Config with Token
 const getConfig = () => {
 
   const token =
@@ -23,30 +15,42 @@ const getConfig = () => {
   }
 }
 
-// Create Note
-export const createNote = async (noteData) => {
+export const getNotes = async () => {
 
-  const response = await axios.post(
-    API_URL,
-    noteData,
-    getConfig()
-  )
-
-  return response.data
-}
-
-// Delete Note
-export const deleteNoteById = async (id) => {
-
-  const response = await axios.delete(
-    `${API_URL}/${id}`,
-    getConfig()
-  )
+  const response =
+    await axios.get(
+      API_URL,
+      getConfig()
+    )
 
   return response.data
 }
 
-// Update Note
+export const createNote =
+  async (noteData) => {
+
+    const response =
+      await axios.post(
+        API_URL,
+        noteData,
+        getConfig()
+      )
+
+    return response.data
+}
+
+export const deleteNoteById =
+  async (id) => {
+
+    const response =
+      await axios.delete(
+        `${API_URL}/${id}`,
+        getConfig()
+      )
+
+    return response.data
+}
+
 export const updateNoteById =
   async (id, noteData) => {
 
@@ -60,7 +64,6 @@ export const updateNoteById =
     return response.data
 }
 
-// Toggle Pin Note
 export const togglePin =
   async (id) => {
 
